c441e72189ee199dabeb79e58e118f3a39cd49b6
Beth <bethmelmtv@gmail.com>
2022-05-18 18:01:47 -0700

    added custom button component

---
+++ src/CustomButton.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export default function CustomButton(text) {
+  return <Button variant="contained"> {text} </Button>;
+}
--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Parade from './Parade.js';
 import OpenSign from './OpenSign.js';
-import LeButton from '@mui/material/LeButton';
+import { CustomButton } from './CustomCustomButton';
 
 function App() {
   const [kenanandkel, setKenanandkelsize] = useState(10);
@@ -17,25 +17,35 @@ function App() {
       <div className="fight">
         <div className="monster">
           <img src="catdog.png" style={{ width: catdog * 10 }} />
-          <div className="LeButton">
-            <LeButton className="LeButton" onClick={() => setCatdogsize(catdog + ATTACK)}>
+          <div className="CustomButton">
+            <CustomButton
+              className="CustomButton"
+              variant="contained"
+              onClick={() => setCatdogsize(catdog + ATTACK)}
+            >
               CatDog did some good deeds!
-            </LeButton>
-            <LeButton className="LeButton" onClick={() => setCatdogsize(catdog - ATTACK)}>
+            </CustomButton>
+            <CustomButton className="CustomButton" onClick={() => setCatdogsize(catdog - ATTACK)}>
               CatDog was sassy to the animals today.
-            </LeButton>
+            </CustomButton>
           </div>
         </div>
 
         <div className="monster">
           <img src="kenanandkel.png" style={{ width: kenanandkel * 10 }} />
-          {/* <div className="LeButton"> */}
-          <LeButton className="LeButton" onClick={() => setKenanandkelsize(kenanandkel + ATTACK)}>
+          {/* <div className="CustomButton"> */}
+          <CustomButton
+            className="CustomButton"
+            onClick={() => setKenanandkelsize(kenanandkel + ATTACK)}
+          >
             Kenan Kel didnt poison the animals
-          </LeButton>
-          <LeButton className="LeButton" onClick={() => setKenanandkelsize(kenanandkel - ATTACK)}>
+          </CustomButton>
+          <CustomButton
+            className="CustomButton"
+            onClick={() => setKenanandkelsize(kenanandkel - ATTACK)}
+          >
             Kenan and Kel did poison some animals by mistake.
-          </LeButton>
+          </CustomButton>
           {/* </div> */}
         </div>
       </div>
@@ -43,21 +53,21 @@ function App() {
         <OpenSign isOpen={isOpen} />
         <h1 className="sign">Welcome to the 90s Zoo! </h1>
         <h2 className="sign">Maintained by your fav 90s all stars!</h2>
-        <div className="LeButton">
-          <LeButton className="LeButton" onClick={() => setisOpen(true)}>
+        <div className="CustomButton">
+          <CustomButton className="CustomButton" onClick={() => setisOpen(true)}>
             Open Zoo!
-          </LeButton>
-          <LeButton className="LeButton" onClick={() => setisOpen(false)}>
+          </CustomButton>
+          <CustomButton className="CustomButton" onClick={() => setisOpen(false)}>
             Close Zoo!
-          </LeButton>
+          </CustomButton>
         </div>
       </div>
 
       <div className="animalparade">
         <Parade animalsEl={animals} />
-        <div className="LeButton">
-          <LeButton
-            className="LeButton"
+        <div className="CustomButton">
+          <CustomButton
+            className="CustomButton"
             onClick={() => {
               animals.push('cat');
               setAnimal([...animals]);
@@ -65,34 +75,34 @@ function App() {
           >
             {' '}
             Add Cat{' '}
-          </LeButton>
-          <LeButton
-            className="LeButton"
+          </CustomButton>
+          <CustomButton
+            className="CustomButton"
             onClick={() => {
               animals.push('dog');
               setAnimal([...animals]);
             }}
           >
             Add Dog
-          </LeButton>
-          <LeButton
-            className="LeButton"
+          </CustomButton>
+          <CustomButton
+            className="CustomButton"
             onClick={() => {
               animals.push('bear');
               setAnimal([...animals]);
             }}
           >
             Add Bear
-          </LeButton>
-          <LeButton
-            className="LeButton"
+          </CustomButton>
+          <CustomButton
+            className="CustomButton"
             onClick={() => {
               animals.push('moose');
               setAnimal([...animals]);
             }}
           >
             Add Moose
-          </LeButton>
+          </CustomButton>
         </div>
       </div>
     </div>
