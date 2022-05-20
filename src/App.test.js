ce3960bfc282525ac129b5b0c9d09c04928f9acf
Beth <bethmelmtv@gmail.com>
2022-05-20 14:52:21 -0700

    updated key in parades.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('when i click addd cat, there are now two cats instead of the single cat in the defaul state', () => {
+test('when i click addd cat, there are now two cats instead of the single cat in the default state', () => {
   render(<App />);
   const catEmojisBefore = screen.queryAllByText(/🐈/i);
   const initialNumberofCats = catEmojisBefore.length;
 
-  const addCatButton = screen.getByText(/Add Cat/i);
+  const addCatButton = screen.getByText(/Add Cat/i); //i is redge ex, which means it only finds the words/ emojis, regardless of capitalization
   fireEvent.click(addCatButton);
   fireEvent.click(addCatButton);
 
@@ -14,7 +14,7 @@ test('when i click addd cat, there are now two cats instead of the single cat in
   expect(catEmojisAfter.length).toBe(initialNumberofCats + 2);
 });
 
-test('when i click on open zoo,Were Open! will appear on the screen ', () => {
+test('when i click on add dog, there are now two dogs instead of the single dog in the defautl state ', () => {
   render(<App />);
   const addDogEmojisBefore = screen.queryAllByText(/🐕/i);
   const initialNumberofDogs = addDogEmojisBefore.length;
--- src/Parade.js
+++ src/Parade.js
@@ -3,8 +3,10 @@ import React from 'react';
 
 export default function Parade({ animalsEl }) {
   return (
-    <div className= 'animals'> 
-      {animalsEl.map((animal) => <AnimalEmoji key={animal} animal={animal} />)}
+    <div className="animals">
+      {animalsEl.map((animal, i) => (
+        <AnimalEmoji key={animal + i} animal={animal} />
+      ))}
     </div>
   );
 }
